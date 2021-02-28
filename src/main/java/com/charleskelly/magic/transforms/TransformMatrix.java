18c23415f9ac3b1b76b214dba6335a5ac8e75b77
fsf2468 <67870707+fsf2468@users.noreply.github.com>
2021-02-28 12:32:01 -1000

    TransformMatrix

---
+++ src/main/java/com/charleskelly/magic/transforms/TransformMatrix.java
@@ -0,0 +1,76 @@
+package com.charleskelly.magic.transforms;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class TransformMatrix
+{
+    private int dimensions;
+    private int[][] matrix;
+
+    public TransformMatrix(int dimensions) throws Exception
+    {
+        this.dimensions = dimensions;
+        matrix = new int[this.dimensions][this.dimensions];
+        prepareTransformMatrix();
+    }
+
+    public void prepareTransformMatrix() throws Exception
+    {
+        try
+        {
+            int index = 0;
+
+            for (int i = 0; i < dimensions; i++)
+            {
+                for (int j = 0; j < dimensions; j++)
+                {
+                    if ((i + j) < dimensions)
+                        matrix[i][j] = 1;
+                    else
+                        matrix[i][j] = -1;
+
+                }
+            }
+            int tempDebug = 0;
+        } catch (Exception e)
+        {
+            log.error(e.toString());
+            throw new Exception(e); // trap for debugging
+        }
+    }
+
+    public int[] getRow(int rowIndex) throws Exception
+    {
+        try
+        {
+            return matrix[rowIndex];
+        }
+        catch (Exception e)
+        {
+            log.error(e.toString() );
+            throw new Exception(e); // trap for debugging
+        }
+    }
+
+
+    public int getDimensions()
+    {
+        return dimensions;
+    }
+
+    public void setDimensions(int dimensions)
+    {
+        this.dimensions = dimensions;
+    }
+
+    public int[][] getMatrix()
+    {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix)
+    {
+        this.matrix = matrix;
+    }
+}
